8cc86b0c253bb9562350f044c89d20f8dd320e7e
aldav99 <aldav99@yandex.ru>
2020-07-30 14:31:54 +0300

    Change BookTable and BookRow

--- src/BookTable.js
+++ src/BookTable.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { BookRow } from './BookRow';
+import { AuthorTable } from './AuthorTable';
+import { SubscribeModal } from './SubscribeModal';
+import { percentOfProgress } from './percentOfProgress';
+
+const styles = {
+    letter: {
+        color: 'red'
+    }
+}
+
 export class BookTable extends React.Component {
     render() {
 
         const books = this.props.books;
-        const rows = books.map((book) => <BookRow book={book} key={book.id} />);
 
         return (
             <table>
@@ -24,8 +33,34 @@ export class BookTable extends React.Component {
                         <th>subscriber</th>
                     </tr>
                 </thead>
-                <tbody>{rows}</tbody>
+                <tbody>
+                    {books.map(function (book, key) {
+                        return (
+                            <BookRow key={book.id}>
+                                <td>{book.title}</td>
+                                <td>{book.brief}</td>
+                                <td>{book.page}</td>
+                                <td>{book.lang}</td>
+                                <td>{percentOfProgress(book.fundedSum, book.neededSum)}</td>
+                                <td><img src={book.cover} width="40"
+                                    height="40"></img></td>
+                                <td><AuthorTable authors={book.authors} /></td>
+
+                                <td>{book.minCost}</td>
+                                <td>{book.neededCost}</td>
+                                <td>{book.fundedSum}</td>
+                                <td>{book.neededSum}</td>
+
+                                {(book.subscriber > 10) ? <td style={styles.letter}>{book.subscriber}</td>
+                                    : <td>{book.subscriber}</td>}
+                                <td><SubscribeModal /></td>
+                            </BookRow>
+                        );
+                    })}
+                </tbody>
             </table>
         );
     }
 }
+
+
