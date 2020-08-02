861bb5fba752c1b5c86d83bbee5cfe9355f02b60
aldav99 <aldav99@yandex.ru>
2020-08-02 09:21:53 +0300

    Class AuthorTable to func

--- src/AuthorTable.js
+++ src/AuthorTable.js
@@ -9,40 +9,36 @@ const AuthorRow = (props) => {
 }
 
 
-export class AuthorTable extends React.Component {
-    render() {
-
-        let authors = Array.from(this.props.authors);
-        if (authors.length > 3) {
-            let result = confirm(`Число авторов ${authors.length}. Вывести всех?`);
-            if (!result) {
-                authors = authors.slice(0, 3);
-            }
+export const AuthorTable = (props) => {
+    let authors = Array.from(props.authors);
+    if (authors.length > 3) {
+        let result = confirm(`Число авторов ${authors.length}. Вывести всех?`);
+        if (!result) {
+            authors = authors.slice(0, 3);
         }
-
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Avatar</th>
-                        <th>Brief</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {authors.map(function (author, key) {
-                        return (
-                            <AuthorRow key={author.id}>
-                                <td>{author.name}</td>
-                                <td>{author.email}</td>
-                                <td><img src={author.avatar} width="40" height="50"></img></td>
-                                <td>{author.brief}</td>
-                            </AuthorRow>
-                        );
-                    })}
-                </tbody>
-            </table>);
     }
-}
 
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Avatar</th>
+                    <th>Brief</th>
+                </tr>
+            </thead>
+            <tbody>
+                {authors.map(function (author, key) {
+                    return (
+                        <AuthorRow key={author.id}>
+                            <td>{author.name}</td>
+                            <td>{author.email}</td>
+                            <td><img src={author.avatar} width="40" height="50"></img></td>
+                            <td>{author.brief}</td>
+                        </AuthorRow>
+                    );
+                })}
+            </tbody>
+        </table>);
+}
