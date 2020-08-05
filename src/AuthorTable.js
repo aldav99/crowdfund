6d06add87c89606b9ffa104d13c854530aaeea0b
aldav99 <aldav99@yandex.ru>
2020-08-05 18:33:50 +0300

    Test

--- src/AuthorTable.js
+++ src/AuthorTable.js
@@ -10,13 +10,17 @@ const AuthorRow = React.memo((props) => {
 
 
 export const AuthorTable = (props) => {
-    let authors = Array.from(props.authors);
-    if (authors.length > 3) {
-        let result = confirm(`Число авторов ${authors.length}. Вывести всех?`);
-        if (!result) {
-            authors = authors.slice(0, 3);
-        }
+    let original = [...props.authors];
+    let authors = [...props.authors];
+    let more = false;
+    function flipFlop(changeView) {
+        changeView = !changeView
     }
+
+    if (more == false) {
+        authors = authors.slice(0, 3);
+    }
+
     console.log('render AuthorTable')
     return (
         <table>
@@ -29,6 +33,11 @@ export const AuthorTable = (props) => {
                 </tr>
             </thead>
             <tbody>
+                {
+                    (props.authors.length > 3) ? <tr><button onClick={() =>
+                        flipFlop(more)}>More...</button></tr> : ''
+                }
+
                 {authors.map(function (author, key) {
                     return (
                         <AuthorRow key={author.id}>
@@ -37,6 +46,7 @@ export const AuthorTable = (props) => {
                             <td><img src={author.avatar} width="40" height="50"></img></td>
                             <td>{author.brief}</td>
                         </AuthorRow>
+
                     );
                 })}
             </tbody>
