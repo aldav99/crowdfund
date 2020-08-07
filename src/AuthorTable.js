55babdb1969ad8e464766a1b3985df831666d22d
aldav99 <aldav99@yandex.ru>
2020-08-07 18:34:46 +0300

    Without PureComponent in AuthorTable

--- src/AuthorTable.js
+++ src/AuthorTable.js
@@ -9,46 +9,51 @@ const AuthorRow = React.memo((props) => {
 })
 
 
-export const AuthorTable = (props) => {
-    let original = [...props.authors];
-    let authors = [...props.authors];
-    let more = false;
-    function flipFlop(changeView) {
-        changeView = !changeView
+
+
+export class AuthorTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { onlyThree: true };
     }
 
-    if (more == false) {
-        authors = authors.slice(0, 3);
+    toggleViev() {
+        this.setState({ onlyThree: !this.state.onlyThree })
     }
 
-    console.log('render AuthorTable')
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Avatar</th>
-                    <th>Brief</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    (props.authors.length > 3) ? <tr><button onClick={() =>
-                        flipFlop(more)}>More...</button></tr> : ''
-                }
+    render() {
+        console.log('render BookRow')
+        let authors;
+        (this.props.authors.length > 3 && this.state.onlyThree) ? authors = this.props.authors.slice(0, 3) : authors = this.props.authors;
 
-                {authors.map(function (author, key) {
-                    return (
-                        <AuthorRow key={author.id}>
-                            <td>{author.name}</td>
-                            <td>{author.email}</td>
-                            <td><img src={author.avatar} width="40" height="50"></img></td>
-                            <td>{author.brief}</td>
-                        </AuthorRow>
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Avatar</th>
+                        <th>Brief</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        (this.props.authors.length > 3) ? <tr><td><button onClick={() =>
+                            this.toggleViev()}>More...</button></td></tr> : <tr></tr>
+                    }
 
-                    );
-                })}
-            </tbody>
-        </table>);
+                    {authors.map(function (author, key) {
+                        return (
+                            <AuthorRow key={author.id}>
+                                <td>{author.name}</td>
+                                <td>{author.email}</td>
+                                <td><img src={author.avatar} width="40" height="50"></img></td>
+                                <td>{author.brief}</td>
+                            </AuthorRow>
+
+                        );
+                    })}
+                </tbody>
+            </table >);
+    }
 }
