10b161dd8bf243924d66908321e42f92ab5c505b
aldav99 <aldav99@yandex.ru>
2020-08-27 06:45:23 +0300

    Add localIdentName

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ module.exports = {
                     loader: 'css-loader',
                     options: {
                         modules: {
-                            auto: true
+                            auto: true,
+                            localIdentName: '[path][name]__[local]--[hash:base64:5]'
                         }
                     }
                 },
