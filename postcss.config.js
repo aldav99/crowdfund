cf76d76cb4080da92ad00c52017f269999acbcd8
aldav99 <aldav99@yandex.ru>
2020-08-28 07:31:03 +0300

    With phone

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('postcss-custom-media')({
+            importFrom: {
+                customMedia: {
+                    '--phone': '(max-width: 541px)'
+                }
+            }
+        })
+    ]
+}
