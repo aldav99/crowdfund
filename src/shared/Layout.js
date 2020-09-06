447f6420402f33183940dfe9e88df33a456d3fd6
aldav99 <aldav99@yandex.ru>
2020-09-06 10:50:38 +0300

    Add Link Logo

--- src/shared/Layout.js
+++ src/shared/Layout.js
@@ -4,13 +4,15 @@ import Logo from './../logo.jpg';
 
 import { UserInfo } from './../UserInfo';
 
+import { Link } from 'react-router-dom';
+
 import styles from "./../style.module.css";
 
 const Layout = (props) => {
     return (
         <React.Fragment>
             <header className={styles.header}>
-                <h2>Crowdfunding</h2>
+                <Link to={'/'}><h2>Crowdfunding</h2></Link>
                 <img src={Logo} width="30"
                     height="30" alt="logo" />
                 <UserInfo />
