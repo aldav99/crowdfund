49f7ce9454f383d01717aa41546fadbf243e04fe
aldav99 <aldav99@yandex.ru>
2020-10-03 18:56:33 +0300

    Delete withBooks HOC

---
+++ src/shared/hooks/useBooks.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios'
+
+const API_TOKEN = 'keyEbYaaHT6MgQv8t'
+
+const httpClient = axios.create({
+    baseURL: 'https://api.airtable.com/v0/appe9eRK1BsGtd1dt',
+    timeout: 15000,
+    headers: {
+        'Authorization': 'Bearer keyEbYaaHT6MgQv8t'
+    }
+});
+
+function _fetchAuthors() {
+    return (
+        httpClient.get('/Authors', {
+            params: {
+                maxRecords: 10,
+                view: 'Grid view'
+            }
+        }).then(result => result.data)
+            .then(_mapFromAirtableAuthors)
+    )
+}
+
+function _fetchData() {
+    return (
+        httpClient.get('/Books', {
+            params: {
+                maxRecords: 10,
+                view: 'Grid view'
+            }
+        }).then(result => result.data)
+            .then(_mapFromAirtable)
+    )
+}
+
+function _mapFromAirtableAuthors(data) {
+    return data.records.map(
+        record => (
+            {
+                name: record.fields.Name,
+                email: record.fields.Email,
+                brief: record.fields.Brief,
+                id: record.fields.Id,
+                idNative: record.id,
+                avatar: record.fields.Avatar[0].thumbnails.large.url
+            }
+        ))
+}
+
+function _mapFromAirtable(data) {
+    return data.records.map(
+        record => (
+            {
+                Id: record.id,
+                id: record.fields.Id,
+                title: record.fields.Title,
+                brief: record.fields.Brief,
+                page: record.fields.Page,
+                lang: record.fields.Lang,
+                cover: record.fields.Cover[0].thumbnails && record.fields.Cover[0].thumbnails.large.url,
+                authors: record.fields.Authors,
+                minCost: record.fields.MinCost,
+                neededCost: record.fields.NeededCost,
+                fundedSum: record.fields.FundedSum,
+                neededSum: record.fields.NeededSum,
+                subscriber: record.fields.Subscriber
+            }
+        )
+    )
+}
+
+
+const useBooks = () => {
+    const [books, setBooks] = useState(null);
+    const [authors, setAuthors] = useState(null);
+
+    useEffect(() => {
+        _fetchAuthors().then(records => { setAuthors(records) }).catch(function (e) { console.log(e) });
+        _fetchData().then(records => { setBooks(records) }).catch(function (e) { console.log(e) });;
+    }, []);
+
+    books && authors && books.map(book => addAuthorsToBook(book, authors))
+
+    return [books, authors];
+};
+
+export default useBooks;
+
+function addAuthorsToBook(book, authors) {
+    book.authorsList = findAuthors(book, authors);
+    return book
+}
+
+function findAuthors(book, authors) {
+    if (!book.authors) return null;
+    authors = authors.filter(author => book.authors.includes(author.idNative))
+
+    return authors
+}
