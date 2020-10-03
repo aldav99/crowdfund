3bcdf6877f2f63b44a62dce2b1bc1115c6cdc806
aldav99 <aldav99@yandex.ru>
2020-10-03 08:31:35 +0300

    Error const CellComponent = column.cell

--- src/BookTable.js
+++ src/BookTable.js
@@ -3,42 +3,42 @@ import { FeedbackForm } from './FeedbackForm';
 import { GenerateTable, Table } from './Refactor';
 
 export const columns = [
-    { Header: '', accessor: 'title', cell: 'LinkCell' },
-    { Header: '', accessor: 'close', cell: 'CloseCell' },
+    { Header: '', accessor: 'title', cell: { LinkCell } },
+    { Header: '', accessor: 'close', cell: { CloseCell } },
     { Header: '', accessor: 'brief' },
     { Header: '', accessor: 'page' },
     { Header: '', accessor: 'lang' },
-    { Header: '', accessor: 'progress', cell: 'ProgressCell' },
-    { Header: '', accessor: 'cover', cell: 'CoverCell' },
-    { Header: '', accessor: 'authors', cell: 'AuthorsCell' },
+    { Header: '', accessor: 'progress', cell: { ProgressCell } },
+    { Header: '', accessor: 'cover', cell: { CoverCell } },
+    { Header: '', accessor: 'authors', cell: { AuthorsCell } },
     { Header: '', accessor: 'minCost' },
-    { Header: '', accessor: 'royalty', cell: 'RoyaltyCell' },
+    { Header: '', accessor: 'royalty', cell: { RoyaltyCell } },
     { Header: '', accessor: 'neededCost' },
     { Header: '', accessor: 'fundedSum' },
     { Header: '', accessor: 'neededSum' },
     {
         Header: '', accessor: 'subscriber',
-        cell: 'SubscriberCell'
+        cell: { SubscriberCell }
     }
 ]
 
 export const mobileColumns = [
-    { Header: 'Title', accessor: 'title', cell: 'LinkCell' },
-    { Header: 'Close', accessor: 'close', cell: 'CloseCell' },
+    { Header: 'Title', accessor: 'title', cell: { LinkCell } },
+    { Header: 'Close', accessor: 'close', cell: { CloseCell } },
     { Header: 'Brief', accessor: 'brief' },
     { Header: 'Page', accessor: 'page' },
     { Header: 'Lang', accessor: 'lang' },
-    { Header: 'Progress', accessor: 'progress', cell: 'ProgressCell' },
-    { Header: 'Cover', accessor: 'cover', cell: 'CoverCell' },
-    { Header: 'Authors', accessor: 'authors', cell: 'AuthorsCell' },
+    { Header: 'Progress', accessor: 'progress', cell: { ProgressCell } },
+    { Header: 'Cover', accessor: 'cover', cell: { CoverCell } },
+    { Header: 'Authors', accessor: 'authors', cell: { AuthorsCell } },
     { Header: 'minCost', accessor: 'minCost' },
-    { Header: 'royalty', accessor: 'royalty', cell: 'RoyaltyCell' },
+    { Header: 'royalty', accessor: 'royalty', cell: { RoyaltyCell } },
     { Header: 'neededCost', accessor: 'neededCost' },
     { Header: 'fundedSum', accessor: 'fundedSum' },
     { Header: 'neededSum', accessor: 'neededSum' },
     {
         Header: 'subscriber', accessor: 'subscriber',
-        cell: 'SubscriberCell'
+        cell: { SubscriberCell }
     }
 ]
 
@@ -72,7 +72,7 @@ export class BookTable extends React.PureComponent {
         const { books, mobileColumns, columns } = this.state;
         return (
             <React.Fragment>
-                <GenerateTable removeFromTable={this.removeFromTable} rows={books} mobileColumns={mobileColumns} columns={columns}/>
+                <GenerateTable removeFromTable={this.removeFromTable} rows={books} mobileColumns={mobileColumns} columns={columns} />
                 <FeedbackForm></FeedbackForm>
             </React.Fragment>
         );
--- src/Refactor.js
+++ src/Refactor.js
@@ -77,13 +77,14 @@ const SubscriberCell = ({ column, row }) => {
         </React.Fragment>)
 }
 
-const componentsMap = { LinkCell, CloseCell, ProgressCell, CoverCell, AuthorsCell, RoyaltyCell, SubscriberCell };
+// const componentsMap = { LinkCell, CloseCell, ProgressCell, CoverCell, AuthorsCell, RoyaltyCell, SubscriberCell };
 
 export const TableRow = ({ row, columns, removeFromTable }) => {
     if (!row) return null;
     return columns.map(column => {
-        const componentName = column.cell;
-        const CellComponent = componentsMap[componentName];
+        // const componentName = column.cell;
+        // const CellComponent = componentsMap[componentName];
+        const CellComponent = column.cell
         return (
             <Td key={column.accessor}>
                 {
