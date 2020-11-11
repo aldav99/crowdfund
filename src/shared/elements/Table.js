c919dbd9df373764342d6bfadc0c38a9a372d729
aldav99 <aldav99@yandex.ru>
2020-11-11 05:06:50 +0300

    Delete Td Tr ... from Table

--- src/shared/elements/Table.js
+++ src/shared/elements/Table.js
@@ -1,81 +1,38 @@
 import React from 'react';
-import styles from "./../styles/style.module.css";
 
 export function Table(props) {
     return (
         <table>
-            <Thead>
-                <Tr>
+            <thead>
+                <tr>
                     {
                         props.columns.map((column, idx) => (
-                            <Th key={idx}>{column.Header}</Th>
+                            <th key={idx}>{column.Header}</th>
                         ))
                     }
-                </Tr>
-            </Thead>
-            <Tbody>
+                </tr>
+            </thead>
+            <tbody>
                 {
                     props.rows.map((row, idx) => (
-                        <Tr key={idx}>
+                        <tr key={idx}>
                             {
                                 props.columns.map((column, idx) => (
-                                    <Td key={idx}>
+                                    <td key={idx}>
                                         {
                                             column.cell
                                                 ? <column.cell row={row} column={column} />
                                                 : row[column.accessor]
                                         }
-                                    </Td>
+                                    </td>
                                 ))
                             }
-                        </Tr>
+                        </tr>
                     ))
                 }
-            </Tbody>
+            </tbody>
         </table>
     );
 };
 
-function Thead(props) {
-    return (
-        <thead>
-            {props.children}
-        </thead>
-    );
-}
-
-export function Tbody(props) {
-    return (
-        <tbody className={styles.Tbody}>
-            {props.children}
-        </tbody>)
-};
-
-export function Td(props) {
-    return (
-        <td className={styles.Td}>
-            {props.children}
-        </td>)
-};
-
-export function Span(props) {
-    return (
-        <span className={styles.Span}>
-            {props.children}
-        </span>)
-};
-
-export function Th(props) {
-    return (
-        <th className={styles.Th}>
-            {props.children}
-        </th>)
-};
-
-export function Tr(props) {
-    return (
-        <tr className={styles.Tr}>
-            {props.children}
-        </tr>)
-};
 
