00106bc9bc6de29b31f75f1e8f98d65ef2024653
aldav99 <aldav99@yandex.ru>
2020-11-25 04:28:44 +0300

    Testing error

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 import { App } from './App';
 
-import { render } from "@testing-library/react"
+import { render, act, screen, waitFor } from "@testing-library/react"
+
+
+import userEvent from "@testing-library/user-event"
 
 describe('Routing', () => {
+    // beforeEach(() => {
+    //     jest.setTimeout(10000);
+    // });
+
     it('It renders Main page correctly', () => {
         const result = render(<App />)
 
         const headerEl = result.getByText('Crowdfunding')
 
         expect(headerEl).toBeTruthy()
+    })
+
+    it('Navigating to Ruby in debt page and back to homepage work correctly', async () => {
+        const { getByText } = render(<App />);
+        // expect(getByText(/Loading/i)).toBeTruthy()
+
+
+        await waitFor(() => {
+            expect(getByText(/Ruby/i)).toBeInTheDocument()
+        })
     })
 });
